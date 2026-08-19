fc6d81122b86905c395304cb9ee960f3000b12e2
nn <roman161rusrosrov@gmail.com>
2026-08-20 02:33:44 +0300

    Configure GitHub Pages: app/robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,13 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
+  const isGitHubPages = process.env.GITHUB_PAGES === "true";
+
   return {
-    rules: [{ userAgent: "*", disallow: "/" }],
+    rules: isGitHubPages
+      ? [{ userAgent: "*", allow: "/" }]
+      : [{ userAgent: "*", disallow: "/" }],
   };
 }
